7641205f295844613ba8df00c6117f78f00e3d83
ENvironmentSet <sbshw1@gmail.com>
2020-05-08 23:56:53 +0900

    Enhance readability of PromotedDataConstructors/List

--- src/PromotedDataConstructors/List.ts
+++ src/PromotedDataConstructors/List.ts
@@ -7,21 +7,26 @@ type _TCons<tcar, tcdr> = PhantomTypeParameter<'TCons/tcar', tcar> & PhantomType
 export interface TCons<tcar, tcdr> extends _TCons<tcar, tcdr> {};
 
 export type Head<tlist> = tlist extends TCons<infer tcar, infer _> ? tcar : Stuck;
+
 export type Tail<tlist> = tlist extends TCons<infer _, infer tcdr> ? tcdr : Stuck;
+
 export type Last<tlist> = {
   base: tlist extends TCons<infer last, infer _> ? last : Stuck;
   recursiveStep: tlist extends TCons<infer _, infer tcdr> ? Last<tcdr> : Stuck;
   undefined: Stuck;
 }[tlist extends TCons<infer _, infer tcdr> ? tcdr extends TNil ? 'base' : 'recursiveStep' : 'undefined'];
+
 export type Init<tlist, result = TNil> = {
   base: result;
   recursiveStep: tlist extends TCons<infer tcar, infer tcdr> ? Init<tcdr, TCons<tcar, result>> : Stuck;
   undefined: Stuck;
 }[tlist extends TCons<infer _, infer tcdr> ? tcdr extends TNil ? 'base' : 'recursiveStep' : 'undefined'];
+
 export type Length<tlist> = {
   base: Z;
   recursiveStep: S<Length<Tail<tlist>>>
 }[tlist extends TNil ? 'base' : 'recursiveStep'];
+
 export type Concat<xs, ys> = {
   base: ys;
   recursiveStep: xs extends TCons<infer tcar, infer tcdr> ? TCons<tcar, Concat<tcdr, ys>> : Stuck;
