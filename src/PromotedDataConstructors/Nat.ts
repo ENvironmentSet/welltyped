46f4dff5249e222bf71e8fa77c0abe4875a01c2b
ENvironmentSet <sbshw1@gmail.com>
2020-05-08 22:52:44 +0900

    Add Type level natural numbers

---
+++ src/PromotedDataConstructors/Nat.ts
@@ -0,0 +1,12 @@
+import { PhantomTypeParameter } from 'Utils/PhantomTypeParameter';
+import { Stuck } from 'Utils/Stuck';
+import { MakeVoid } from 'Utils/MakeVoid';
+
+export interface Z extends MakeVoid<'Z'> {};
+type _S<Nat> = PhantomTypeParameter<'Nat', Nat>;
+export interface S<Nat> extends _S<Nat> {};
+
+export type Add<X, Y> = {
+  base: Y;
+  recursiveStep: X extends S<infer XP> ? Add<XP, S<Y>> : Stuck;
+}[X extends Z ? 'base' : 'recursiveStep'];
