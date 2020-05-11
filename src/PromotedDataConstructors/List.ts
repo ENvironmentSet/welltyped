9c8a0f8133d3c7f1c4d451a18dd9c423abc8a2a2
ENvironmentSet <sbshw1@gmail.com>
2020-05-12 01:55:06 +0900

    Remove Nil, Add concat and Filter

--- src/PromotedDataConstructors/List.ts
+++ src/PromotedDataConstructors/List.ts
@@ -7,9 +7,6 @@ import { Z, S } from 'PromotedDataConstructors/Nat';
 
 export type AnyList = Array<Type>;
 
-type Nil_ = [];
-export interface Nil extends Nil_ {}
-
 export interface IsEmpty extends HKT {
   result: this['param'] extends [] ? true : false;
 }
@@ -29,7 +26,7 @@ export interface Length extends HKT {
   result: this['param'] extends infer list ? {
       base: Z;
       recursiveStep: S<Apply<Length, Apply<Tail, list>>>
-    }[list extends [] ? 'base' : 'recursiveStep']
+    }[Apply<If, [Apply<IsEmpty, list>, 'base', 'recursiveStep']>]
     : Stuck
 }
 
@@ -37,4 +34,34 @@ export interface Cons extends HKT {
   result: this['param'] extends [infer car, infer cdr] ?
     cdr extends AnyList ?
       ((car: car, ...cdr: cdr) => never) extends (..._: infer consed) => never ? consed : Stuck : Stuck : Stuck;
+}
+
+export interface Filter extends HKT {
+  result: this['param'] extends [infer predicate, infer list] ? {
+    base: [],
+    recursiveStep: predicate extends HKT ?
+      list extends AnyList ?
+        Apply<If, [
+          Apply<predicate, Apply<Head, list>>,
+          Apply<Filter, [predicate, Apply<Tail, list>]> extends infer xs ?
+            Apply<Cons, [Apply<Head, list>, xs]>
+            : Stuck,
+          Apply<Filter, [predicate, Apply<Tail, list>]>
+        ]>
+        : Stuck
+      : Stuck
+  }[Apply<If, [Apply<IsEmpty, list>, 'base', 'recursiveStep']>]
+  : Stuck;
+}
+
+export interface Concat extends HKT {
+  result: this['param'] extends [infer xs, infer ys] ? {
+      base: ys,
+      recursiveStep: xs extends AnyList ?
+        ys extends AnyList ?
+            Apply<Cons, [Apply<Head, xs>, Apply<Concat, [Apply<Tail, xs>, ys]>]>
+          : Stuck
+        : Stuck
+    }[Apply<If, [Apply<IsEmpty, xs>, 'base', 'recursiveStep']>]
+    : Stuck;
 }
