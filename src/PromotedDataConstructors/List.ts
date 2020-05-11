7d9a306b6d436b35b297d0684ced958bd98c5eeb
ENvironmentSet <sbshw1@gmail.com>
2020-05-11 23:36:10 +0900

    Refactor List
    - Many strange things are happened while writing this code.
    
    here is my note:
    
    1. HKTs are seems not compatible with self-appointed circular conditional type.
    2. Sometimes, Context of HKTs' param is lost.
    3. Sometimes, HKTs are work unexpectedly, and it results never or some odd type(usually super-narrowed types)
    3.1 Getting HKTs' result kind results super-narrowed type, why?
    and it seems undecidable(go check Not HKTs and GetHKTInfos)
    3.2 HKTs work unintelligibly when we uses this['param'] with function types, go check List.ts's comments
    3.3 also in apply operation, f & { param: p } extends { failed: true } ? true : false is always true, why?
    
    conclusion: Investigation about HKTs' fundamentals is heavily required.

--- src/PromotedDataConstructors/List.ts
+++ src/PromotedDataConstructors/List.ts
@@ -1,49 +1,30 @@
-import { Stuck } from 'Utils';
-import {
-  RawTCons, RawTNil,
-  RawHead, RawTail, RawLength,
-  RawInit, RawLast, RawConcat
-} from 'PromotedDataConstructors/RawList';
+import { Stuck } from 'Utils/Stuck';
+import { Type } from 'Utils/Type';
+import { HKT, TotalHKT } from 'Utils/HKT';
+import { Apply } from 'Utils/Apply';
+import { If } from 'PromotedDataConstructors/Bool';
 
-export type AnyTListKind = unknown[];
+export type AnyList = Array<Type>;
 
-// Inspired by $mol_type https://github.com/eigenmethod/mol/tree/master/type
-export type ToRawList<list extends AnyTListKind> = {
-  base: RawTNil;
-  recursiveStep: ((...tlist: list) => never) extends ((car: infer car, ...cdr: infer cdr) => never) ?
-    ToRawList<cdr> extends infer rawCdr ?
-      RawTCons<car, rawCdr>
-      : Stuck
-    : Stuck;
-}[list extends [] ? 'base' : 'recursiveStep'];
+export interface IsEmpty extends TotalHKT<AnyList> {
+  result: this['param'] extends [] ? true : false;
+}
 
-export type FromRawList<rawList> = {
-  base: [];
-  recursiveStep: rawList extends RawTCons<infer car, infer rawCdr> ?
-    FromRawList<rawCdr> extends infer cdr ?
-      cdr extends AnyTListKind ?
-        ((car: car, ..._cdr: cdr) => never) extends ((...list: infer list) => never) ?
-          list
-          : Stuck
+export interface Head extends HKT<AnyList> {
+  result: Apply<If, [Apply<IsEmpty, this['param']>, Stuck, this['param'][0]]>;
+  failed: Apply<IsEmpty, this['param']>;
+}
+
+export interface Tail extends HKT { //@FIXME: without not specifying type of param, this doesn't works. further investigation required.
+  result: this['param'] extends AnyList ?
+      ((..._: this['param']) => never) extends (_: infer _, ...tail: infer tail) => never ? tail
         : Stuck
-      : Stuck
     : Stuck;
-}[rawList extends RawTNil ? 'base' : 'recursiveStep'];
+  failed: this['param'] extends AnyList ? Apply<IsEmpty, this['param']> : Stuck;
+}
 
-export type Head<list extends AnyTListKind> = RawHead<ToRawList<list>>;
-
-export type Tail<list extends AnyTListKind> = FromRawList<RawTail<ToRawList<list>>>;
-
-export type Last<list extends AnyTListKind> = RawLast<ToRawList<list>>;
-
-export type Init<list extends AnyTListKind> = FromRawList<RawInit<ToRawList<list>>>;
-
-export type Length<list extends AnyTListKind> = RawLength<ToRawList<list>>;
-
-export type Concat<xs extends AnyTListKind, ys extends AnyTListKind>
-  = FromRawList<RawConcat<ToRawList<xs>, ToRawList<ys>>>;
-
-export type Cons<car, cdr extends AnyTListKind> = FromRawList<RawTCons<car, ToRawList<cdr>>>
-
-type _Nil = [];
-export interface Nil extends _Nil {};
+export interface Cons extends TotalHKT { //@FIXME: without not specifying type of param, this doesn't works. further investigation required.
+  result: this['param'] extends [infer car, infer cdr] ?
+    cdr extends AnyList ?
+      ((car: car, ...cdr: cdr) => never) extends (..._: infer consed) => never ? consed : Stuck : Stuck : Stuck;
+}
