7dc67240de9186d47af5aa9f1d4276c57f3eb5c8
ENvironmentSet <sbshw1@gmail.com>
2020-05-12 01:35:42 +0900

    Fix Apply

--- src/Utils/Apply.ts
+++ src/Utils/Apply.ts
@@ -1,5 +1,5 @@
 import { HKT } from 'Utils/HKT';
 import { Stuck } from 'Utils/Stuck';
 
-export type Apply<f extends HKT, x extends f['param'], fallback = Stuck>
+export type Apply<f extends HKT, x, fallback = Stuck>
   = (f & { param: x })['result'] | fallback;
