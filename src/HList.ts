2574a26e40c0ce75935c7649983072b397d2ab0b
ENvironmentSet <sbshw1@gmail.com>
2020-05-13 10:24:34 +0900

    Refactor

--- src/HList.ts
+++ src/HList.ts
@@ -1,9 +1,8 @@
 import { Cons, Head, AnyList, Tail, Concat } from 'PromotedDataConstructors/List';
-import { Apply } from 'Utils/Apply';
 
 export const hnil = [] as [];
-export function hcons<type, tlist extends AnyList>(x: type, xs: tlist): Apply<Cons, [type, tlist]> {
-  return [x, ...xs] as Apply<Cons, [type, tlist]>;
+export function hcons<type, tlist extends AnyList>(x: type, xs: tlist): Cons<[type, tlist]> {
+  return [x, ...xs] as Cons<[type, tlist]>;
 }
 
 export function hl<tlist extends AnyList>(_: TemplateStringsArray, ...xs: tlist): tlist;
@@ -13,14 +12,14 @@ export function hl(x: any, ...xs: any) {
   else return [x, ...xs];
 }
 
-export function head<tlist extends AnyList>([x]: tlist): Apply<Head, tlist> {
-  return x as Apply<Head, tlist>;
+export function head<tlist extends AnyList>([x]: tlist): Head<tlist> {
+  return x as Head<tlist>;
 }
 
-export function tail<tlist extends AnyList>([_, ...xs]: tlist): Apply<Tail, tlist> {
-  return xs as Apply<Tail, tlist>;
+export function tail<tlist extends AnyList>([_, ...xs]: tlist): Tail<tlist> {
+  return xs as Tail<tlist>;
 }
 
-export function concat<xs extends AnyList, ys extends AnyList>(xs: xs, ys: ys): Apply<Concat, [xs, ys]> {
-  return xs.concat(ys) as Apply<Concat, [xs, ys]>;
+export function concat<xs extends AnyList, ys extends AnyList>(xs: xs, ys: ys): Concat<[xs, ys]> {
+  return xs.concat(ys) as Concat<[xs, ys]>;
 }
