95adb0b1aa6b2fb9a729ea821aae8611ef08c509
ENvironmentSet <sbshw1@gmail.com>
2020-05-13 09:59:02 +0900

    Add IsUnInitialized function

--- src/Utils/UnInitialized.ts
+++ src/Utils/UnInitialized.ts
@@ -1,3 +1,10 @@
 import { MakeVoid } from 'Utils/MakeVoid';
+import { HKT } from 'Utils/HKT';
+import { Apply } from 'Utils/Apply';
 
 export interface UnInitialized extends MakeVoid<'UnInitialized'> {}
+
+interface _IsUnInitialized extends HKT {
+  result: this['param'] extends UnInitialized ? true : false;
+}
+export type IsUnInitialized<param> = param extends UnInitialized ? _IsUnInitialized : Apply<_IsUnInitialized, param>;
