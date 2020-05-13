165b9d1a3999936e7316c46f94dfff0ed4a54472
ENvironmentSet <sbshw1@gmail.com>
2020-05-13 10:06:20 +0900

    Add DeriveGeneric

--- src/Utils/UnInitialized.ts
+++ src/Utils/UnInitialized.ts
@@ -8,3 +8,5 @@ interface _IsUnInitialized extends HKT {
   result: this['param'] extends UnInitialized ? true : false;
 }
 export type IsUnInitialized<param> = param extends UnInitialized ? _IsUnInitialized : Apply<_IsUnInitialized, param>;
+
+export type DeriveGeneric<f extends HKT, param> = param extends UnInitialized ? f : Apply<f, param>;
