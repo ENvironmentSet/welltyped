f45bbb495aa2e2e94818a685ed2a3d9989421c7a
ENvironmentSet <sbshw1@gmail.com>
2020-05-13 09:59:26 +0900

    Refactor Bool.ts with UnInitialized module

--- src/PromotedDataConstructors/Bool.ts
+++ src/PromotedDataConstructors/Bool.ts
@@ -1,12 +1,16 @@
 import { HKT } from 'Utils/HKT';
 import { Stuck } from 'Utils/Stuck';
+import { IsUnInitialized, UnInitialized } from 'Utils/UnInitialized';
+import { Apply } from 'Utils/Apply';
 
-export interface If extends HKT {
+interface _If extends HKT {
   result: this['param'] extends [infer condition, infer then, infer orElse] ?
     condition extends true ? then : orElse
     : Stuck;
 }
+export type If<param = UnInitialized> = Apply<_If, [IsUnInitialized<param>, _If, Apply<_If, param>]>;
 
-export interface Not extends HKT {
+interface _Not extends HKT {
   result: this['param'] extends true ? false : true;
 }
+export type Not<param> = If<[IsUnInitialized<param>, _Not, Apply<_Not, param>]>;
