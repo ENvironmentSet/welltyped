cb6bbb99c064f84566a26f2648068418fc95a2f5
ENvironmentSet <sbshw1@gmail.com>
2020-05-13 10:06:32 +0900

    Refactor with DeriveGeneric

--- src/PromotedDataConstructors/Bool.ts
+++ src/PromotedDataConstructors/Bool.ts
@@ -1,16 +1,15 @@
 import { HKT } from 'Utils/HKT';
 import { Stuck } from 'Utils/Stuck';
-import { IsUnInitialized, UnInitialized } from 'Utils/UnInitialized';
-import { Apply } from 'Utils/Apply';
+import { UnInitialized, DeriveGeneric } from 'Utils/UnInitialized';
 
 interface _If extends HKT {
   result: this['param'] extends [infer condition, infer then, infer orElse] ?
     condition extends true ? then : orElse
     : Stuck;
 }
-export type If<param = UnInitialized> = Apply<_If, [IsUnInitialized<param>, _If, Apply<_If, param>]>;
+export type If<param = UnInitialized> = DeriveGeneric<_If, param>;
 
 interface _Not extends HKT {
   result: this['param'] extends true ? false : true;
 }
-export type Not<param> = If<[IsUnInitialized<param>, _Not, Apply<_Not, param>]>;
+export type Not<param> = DeriveGeneric<_Not, param>;
