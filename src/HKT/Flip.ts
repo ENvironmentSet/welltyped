d498ffb0a52a8ceebca5da567b8c138ba826cb0a
ENvironmentSet <sbshw1@gmail.com>
2020-05-14 09:46:23 +0900

    Add Derived generic of Flip

--- src/HKT/Flip.ts
+++ src/HKT/Flip.ts
@@ -1,12 +1,14 @@
 import { HKT, Apply } from 'Primitive/HKT';
 import { Stuck } from 'Primitive/Stuck';
 import { Reverse } from 'Promoted/List';
+import { DeriveGeneric, UnInitialized } from 'Primitive/UnInitialized';
 
 interface Flipped<f extends HKT> extends HKT {
   result: Apply<f, Apply<Reverse, this['param']>>;
 }
-export interface Flip extends HKT {
+interface _Flip extends HKT {
   result: this['param'] extends HKT ?
     Flipped<this['param']>
     : Stuck;
 }
+export type Flip<param = UnInitialized> = DeriveGeneric<_Flip, param>;
