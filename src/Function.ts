d6bfa4aa88283287d13713fe655656de1bcbbbec
ENvironmentSet <sbshw1@gmail.com>
2020-05-14 10:29:50 +0900

    Refactor type `Curried`

--- src/Function.ts
+++ src/Function.ts
@@ -1,68 +1,52 @@
-import { Length, Reduce, AnyList, Map, Head, Tail, Snoc, Last, Cons } from 'Promoted/List';
+import { Length, Reduce, AnyList, Map, Head, Tail, Snoc, Init, Cons, Scan } from 'Promoted/List';
 import { hnil } from 'HList';
 import { Z, S } from 'Promoted/Nat';
 import { Stuck } from 'Primitive/Stuck';
 import { HKT, Apply } from 'Primitive/HKT';
 import { DeriveGeneric, UnInitialized } from 'Primitive/UnInitialized';
 import { Intersection } from 'Promoted/Intersection';
+import { Flip } from 'HKT/Flip';
 
 export type AnyFunction = (...args: never) => unknown;
 
-type Diff<A, B> = {
+type FilterByMatch<A, B> = {
   base: B;
-  recursiveStep: Head<A> extends Head<B> ?
-    Diff<Tail<A>, Tail<B>>
-    : Cons<[Head<B>, Diff<Tail<A>, Tail<B>>]>
-}[Length<A> extends Z ? 'base' : A extends B ? B extends A ? 'base' : 'recursiveStep' : 'recursiveStep'];
+  recursiveStep: Head<A> extends (Head<B> & Head<A>) ?
+    FilterByMatch<Tail<A>, Tail<B>>
+    : Cons<[Head<B>, FilterByMatch<Tail<A>, Tail<B>>]>
+}[Length<A> extends Z ? 'base' : 'recursiveStep'];
 
-interface _Lambda_2C<curriedParams> extends HKT {
-  result: this['param'] extends AnyList ?
-    curriedParams extends [infer originalParams, infer returnType] ?
-      Diff<this['param'], originalParams> extends infer A ?
+interface ExtractNArgFunction<functionSignature extends AnyFunction> extends HKT {
+  result:
+    this['param'] extends AnyList ?
+      FilterByMatch<this['param'], Parameters<functionSignature>> extends infer A ?
         A extends AnyList ?
-          (...args: this['param']) => Curried<(...args: A) => returnType>
+          (...args: this['param']) => Curried<(...args: A) => ReturnType<functionSignature>>
           : Stuck
         : Stuck
-      : Stuck
     : Stuck;
 }
-interface _Lambda_2 extends HKT {
-  result: _Lambda_2C<this['param']>;
-}
-type Lambda_2<param = UnInitialized> = DeriveGeneric<_Lambda_2, param>;
 
-interface Lambda_1 extends HKT {
-  result: this['param'] extends [infer acc, infer type] ?
-    Snoc<[
-      Snoc<[type, Last<acc>]>
-      ,
-      acc
-    ]>
-    : Stuck;
-}
-type MagicalSlice<list extends AnyList> = Reduce<[
-  Lambda_1,
-  [[Head<list>]],
-  Tail<list>
-]>;
+type Subsequences<list extends AnyList> = Init<Scan<[
+  Flip<Snoc>,
+  [],
+  list
+]>>;
 
 interface _Curried extends HKT {
   result: this['param'] extends infer f ?
     f extends AnyFunction ? {
         base: Length<Parameters<f>> extends Z ? ReturnType<f> : f;
-        recursiveStep: Parameters<f> extends infer params ?
-          params extends AnyList ?
+        recursiveStep:
             Reduce<[
               Intersection,
               unknown,
               Map<[
-                Lambda_2<[params, ReturnType<f>]>
+                ExtractNArgFunction<f>
                 ,
-                MagicalSlice<params>
+                Subsequences<Parameters<f>>
               ]>
             ]>
-            : Stuck
-          : Stuck;
       }[Length<Parameters<f>> extends Z | S<Z> ? 'base' : 'recursiveStep']
       : Stuck
     : Stuck;
