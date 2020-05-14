5b6bf69ad8726f9872369541cb0221a74aca9661
ENvironmentSet <sbshw1@gmail.com>
2020-05-15 00:44:24 +0900

    Curry Eq

--- src/Promoted/Eq.ts
+++ src/Promoted/Eq.ts
@@ -1,7 +1,9 @@
 import { HKT } from '../Primitive/HKT';
-import { Stuck } from '../Primitive/Stuck';
 import { DeriveGeneric, UnInitialized } from '../Primitive/UnInitialized';
 
+interface _EqC<A> extends HKT {
+  result: Eq<[A, this['param']]>;
+}
 interface _Eq extends HKT {
   result: this['param'] extends [infer A, infer B] ?
     A extends B ?
@@ -9,6 +11,6 @@ interface _Eq extends HKT {
         true
         : false
       : false
-    : Stuck;
+    : _EqC<this['param']>;
 }
 export type Eq<param = UnInitialized> = DeriveGeneric<_Eq, param>;
