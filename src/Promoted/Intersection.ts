5779fc1541b3d22dfd1be457f88f87089dee319b
ENvironmentSet <sbshw1@gmail.com>
2020-09-16 00:13:46 +0900

    Refactor Intersection

--- src/Promoted/Intersection.ts
+++ src/Promoted/Intersection.ts
@@ -3,8 +3,8 @@ import { Stuck } from '../Primitive/Stuck';
 import { DeriveGeneric, UnInitialized } from '../Primitive/UnInitialized';
 
 interface _Intersection extends HKT {
-  result: this['param'] extends [infer A, infer B] ?
-    A & B
+  result: this['param'] extends [infer a, infer b] ?
+    a & b
     : Stuck;
 }
 export type Intersection<param = UnInitialized> = DeriveGeneric<_Intersection, param>;
