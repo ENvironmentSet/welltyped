b9a6a03bb1a824f5eaa042b4d35216455121ccd2
ENvironmentSet <sbshw1@gmail.com>
2020-09-16 01:30:22 +0900

    Add TObject

--- src/Promoted/TObject.ts
+++ src/Promoted/TObject.ts
@@ -1,2 +1,10 @@
+import { TString } from './TString';
+import { TType } from '../Primitive/TType';
+
+//@TODO Make type families first class.
+
 export type TObject = object;
 export type Empty = {};
+
+export type Set<tobject extends TObject, propName extends TString, propValue extends TType>
+  = Pick<tobject, Exclude<keyof tobject, propName>> & Record<propName, propValue>;
