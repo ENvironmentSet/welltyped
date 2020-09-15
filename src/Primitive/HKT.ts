c02f2887447b87cb5742b7f27298286c573bfe1a
ENvironmentSet <sbshw1@gmail.com>
2020-09-15 23:39:16 +0900

    Replace Type by TType

--- src/Primitive/HKT.ts
+++ src/Primitive/HKT.ts
@@ -1,9 +1,9 @@
-import { Type } from './Type';
+import { TType } from './TType';
 import { Stuck } from './Stuck';
 
 export interface HKT {
-  param: Type;
-  result: Type;
+  param: TType;
+  result: TType;
 }
 
 export type Apply<f extends HKT, x, fallback = Stuck> //@TODO: Make Apply non-primitive
