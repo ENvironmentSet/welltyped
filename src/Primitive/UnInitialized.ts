8d3eb76247ec76919b3aa4f5e8a397fc31eff12e
ENvironmentSet <sbshw1@gmail.com>
2020-09-16 14:34:17 +0900

    delete verbose IsUnInitialized

--- src/Primitive/UnInitialized.ts
+++ src/Primitive/UnInitialized.ts
@@ -3,9 +3,4 @@ import { HKT, Apply } from './HKT';
 
 export interface UnInitialized extends MakeVoid<'UnInitialized'> {}
 
-interface _IsUnInitialized extends HKT {
-  result: this['param'] extends UnInitialized ? true : false;
-}
-export type IsUnInitialized<param> = param extends UnInitialized ? _IsUnInitialized : Apply<_IsUnInitialized, param>;
-
 export type DeriveGeneric<f extends HKT, param> = param extends UnInitialized ? f : Apply<f, param>;
