99b84a6e98bed581482aabd572aab30869786a69
ENvironmentSet <sbshw1@gmail.com>
2020-09-16 20:20:20 +0900

    Update Array

--- src/Array.ts
+++ src/Array.ts
@@ -1,20 +1,18 @@
-import { Empty, TArray, Cons, Concat } from './Promoted/TArray';
+import { TArray, Cons, Concat } from './Promoted/TArray';
 import { TType } from './Primitive/TType';
 
-export const empty = [] as Empty;
-
-export function cons<type extends TType, tarray extends TArray>(x: type, xs: tarray): Cons<[type, tarray]> {
-  return [x, ...xs] as Cons<[type, tarray]>;
+export function cons<type extends TType, tarray extends TArray>(x: type, xs: tarray): Cons<type, tarray> {
+  return [x, ...xs];
 }
 
-export function head<type extends TType>([x]: Cons<[type, TArray]>): type {
+export function head<type extends TType>([x]: Cons<type, TArray>): type {
   return x;
 }
 
-export function tail<tarray extends TArray>([_, ...xs]: Cons<[unknown, tarray]>): tarray {
-  return xs as tarray;
+export function tail<tarray extends TArray>([_, ...xs]: Cons<TType, tarray>): tarray {
+  return xs;
 }
 
-export function concat<xs extends TArray, ys extends TArray>(xs: xs, ys: ys): Concat<[xs, ys]> {
-  return xs.concat(ys) as Concat<[xs, ys]>;
+export function concat<xs extends TArray, ys extends TArray>(xs: xs, ys: ys): Concat<xs, ys> {
+  return [...xs, ...ys];
 }
