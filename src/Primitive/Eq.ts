9ec76fef66ff7554a8fa1fd7bc76a87991ceaf83
ENvironmentSet <sbshw1@gmail.com>
2022-01-06 13:06:03 +0900

    refactor Eq

--- src/Primitive/Eq.ts
+++ src/Primitive/Eq.ts
@@ -1,7 +1,9 @@
 import { HKT } from '../Primitive/HKT';
 import { TType } from '../Primitive/TType';
 
-export type Eq<a, b> = a extends b ? b extends a ? true : false : false;
+export type Eq<a, b> =
+  (<T>() => T extends a ? never : never) extends (<T>() => T extends b ? never : never) ?
+    true : false;
 export interface Eq_ extends HKT {
   params: [TType, TType];
   a: this['params'][0];
